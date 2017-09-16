e3653be6c2177abfe6a982c5aeeef411aea20341
ZOOL <www.iphone@foxmail.com>
2017-09-16 23:20:24 +0800

    del bin

--- src/day05/ArrayDemo4.java
+++ src/day05/ArrayDemo4.java
@@ -2,8 +2,46 @@ public class ArrayDemo4{
 
     public static void main(String[] args) {
         int [] arr = new int []{1,4,67,12,44,10,90,91,10};
-        int max = getMax(arr);
-        System.out.println(max);
+        //int max = getMax(arr);
+        //System.out.println(max);
+        sort(arr);
+        // for(int i=0; i<arr.length; i++){
+        //     System.out.println(arr[i]);
+        // }
+        printArray(arr);
+    }
+
+    //打印数组
+    public static void printArray(int arr[]){
+
+        System.out.print("[ ");
+        for(int i=0; i<arr.length; i++){
+
+            if(i==arr.length){
+                System.out.println(arr[i]+"]");
+            }else{
+                System.out.print(arr[i]+", ");
+            }
+        }
+
+    }
+
+    //排序数组，按从小到大的顺序排列
+    public static void sort(int []arr){
+
+        for(int i=0; i<arr.length-1; i++){
+            
+            for(int x=i+1; x<arr.length; x++){
+
+                if(arr[i]>arr[x]){
+                    int temp=arr[x];
+                    arr[x]=arr[i];
+                    arr[i]=temp;
+                }
+
+            }
+
+        }
 
     }
 

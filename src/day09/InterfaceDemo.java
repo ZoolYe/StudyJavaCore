8fafea49fedd7bc844e9c463614ca5268fc39527
ZOOL <www.iphone@foxmail.com>
2017-09-26 18:37:25 +0800

    add interface file

--- src/day09/InterfaceDemo.java
+++ src/day09/InterfaceDemo.java
@@ -1,6 +1,11 @@
 public class InterfaceDemo{
     public static void main(String[] args) {
-    
+        DemoImpl d = new DemoImpl();
+        /* System.out.println(d.NUM);
+        System.out.println(DemoImpl.NUM);
+        System.out.println(Demo.NUM); */
+        Test2 t = new Test2();
+        t.show();
     }
 }
 /* 
@@ -16,4 +21,90 @@ interface Demo{
     public abstract void show1();
     public abstract void show2();
 
+}
+
+interface A{
+    public abstract void show();
+}
+
+interface B{
+    public abstract int add(int a,int b);
+}
+
+class Test implements A,B{
+    public void show(){
+
+    }
+    public int add(int a,int b){
+        return a+b;
+    }
+}
+
+/* 
+    类与类之间是继承关系，类与接口之间是实现关系
+    接口不可以实例化
+    只能由实现了接口的子类并覆盖了接口中所有的抽象方法后，该子类才可以实例化
+    否则这个子类就是一个抽象类
+*/
+
+
+class DemoImpl implements Demo{
+
+    public void show1(){
+
+    }
+
+    public void show2(){
+
+    }
+}
+/* 
+    在Java中不直接支持多继承，因为会出现调用的不确定性
+    所以Java将多继承机制进行了改良，在Java中变成了多实现
+    一个类可以实现多个接口
+    一个类在继承另一个类的同时，还可以实现多个接口
+*/
+class Q{
+    void function(){
+        System.out.println("Q...");
+    }
+}
+
+class Test2 extends Q implements A,B{
+    public void show(){
+        super.function();
+    }
+
+    public int add(int a, int b){
+        return a+b;
+    }
+}
+
+//接口与接口之间是继承关系
+interface CC{
+    public abstract void show1();
+}
+
+interface BB{
+    public abstract void show2();
+}
+
+interface QQ extends BB,CC{
+    public abstract void show3();
+}
+
+class HH implements QQ{
+
+    //需要覆盖三个方法
+    public void show1(){
+
+    }
+
+    public void show3(){
+
+    }
+
+    public void show2(){
+
+    }
 }

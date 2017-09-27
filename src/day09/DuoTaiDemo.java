5b999c5d0a207be7eaf9251ae6f402aff6951fc8
ZOOL <www.iphone@foxmail.com>
2017-09-27 22:57:54 +0800

    add new duotai file

--- src/day09/DuoTaiDemo.java
+++ src/day09/DuoTaiDemo.java
@@ -1,10 +1,16 @@
 public class DuoTaiDemo{
     public static void main(String[] args) {
-        Cat c = new Cat();
+        /* Cat c = new Cat();
         animalEat(c);
-
         Dog d = new Dog();
-        animalEat(d);
+        animalEat(d); */
+
+        Animal a = new Cat();//自动类型提升，猫对象提升了动物类型，但是特有功能无法访问  向上转型
+        a.eat();
+
+        //向下转型,向下转型的目的是为了使用子类中的特有方法
+        Cat c = (Cat)a;
+        c.catchMice();
     }
 
     public static void animalEat(Animal a){
@@ -16,6 +22,9 @@ public class DuoTaiDemo{
     对象的多态性：一个对象对应着不同的类型
     多态在代码中的体现：父类或者接口的引用指向其子类的对象
     多态的好处：提高了代码的扩展性，前期定义的代码可以使用后期的内容
+    多态的弊端：前期定义的内容，不能使用后期子类的特有内容
+    使用多态时的前提：1，必须有关系，继承，实现 2，要有覆盖
+    注意：对于转型，自始至终都是子类对象在做着类型的变化
 */
 
 abstract class Animal{

479e057106422138d82adb55bfcc38f065b25830
ZOOL <www.iphone@foxmail.com>
2017-10-01 18:58:22 +0800

    new add exception

---
+++ src/day11/ExceptionDemo3.java
@@ -0,0 +1,10 @@
+public class ExceptionDemo3{
+    public static void main(String[] args) {
+        
+    }
+}
+/* 
+    对于角标是正数不存在，可以用角标越界来表示
+    对于负数为角标的情况，准备用负数角标异常来表示
+    负数角标这种异常在Java中并没有定义过  只能自定义,这种自定义的问题描述称为自定义异常
+*/

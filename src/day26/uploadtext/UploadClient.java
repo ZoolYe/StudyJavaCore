36d839c4e3443340528a5cf576a7ed8e8e52841a
ZOOL <www.iphone@foxmail.com>
2017-12-09 13:58:30 +0800

    set upload file

--- src/day26/uploadtext/UploadClient.java
+++ src/day26/uploadtext/UploadClient.java
@@ -12,31 +12,38 @@ public class UploadClient {
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		
 		//1，创建客户端Socket端点
-		Socket s = new Socket("192.168.0.102", 6248);
+		Socket s = new Socket("192.168.0.101", 6248);
 		
-		//2，创建数据源
+		//2，创建数据源，从硬盘上读取文件
 		BufferedInputStream bufis = new BufferedInputStream(new FileInputStream("C:\\Users\\ZOOL\\Desktop\\浙江.pptx"));
 		
+		//3，获取Socket输出字节流，将字节流加上字节流缓冲区
 		BufferedOutputStream bufos = new BufferedOutputStream(s.getOutputStream());
 		
+		//创建缓冲区，每次读取1024个字节
 		byte []buf = new byte[1024];
+		//记录上次读取的位置
 		int len = 0;
+		//循环读取文件中的数据
 		while((len = bufis.read(buf))!=-1) {
+			//将数据写到Socket输出流中
 			bufos.write(buf, 0, len);
+			//刷新数据
 			bufos.flush();
-			System.out.println(len);
 		}
-		System.out.println("执行到我了");
-		bufos.write("over".getBytes());
-		bufos.flush();
-		System.out.println("我也执行打了");
+		//告诉服务器，输出流结束了
+		s.shutdownOutput();
 		
+		//获取Socket输入流，获取服务器返回的数据
 		BufferedInputStream refis = new BufferedInputStream(s.getInputStream());
+		//创建缓冲区，用于存储，服务器返回的数据
 		byte []rebuf = new byte[1024];
-		//int len =
+		//读取服务器返回的数据
 		int relen = refis.read(rebuf);
+		//将服务器返回的数据转成字符串
 		String str = new String(rebuf,0,relen);
-		System.out.println(str);
+		System.out.println("客户端"+str);
+		//关闭资源
 		refis.close();
 		s.close();
 	}
--- src/day26/uploadtext/UploadServer.java
+++ src/day26/uploadtext/UploadServer.java
@@ -13,28 +13,35 @@ public class UploadServer {
 
 	public static void main(String[] args) throws IOException {
 
+		//创建服务端Socket对象，设置端口
 		ServerSocket ss = new ServerSocket(6248);
-	
+		//获取Socket端点
 		Socket s = ss.accept();
-		
+		//获取IP对象
 		InetAddress ip = s.getInetAddress();
+		//获取ip地址
 		String ip_add = ip.getHostAddress();
+		//获取端口
 		int port = s.getPort();
 		
+		//获取Socket端点的输入字节流
 		BufferedInputStream bufis = new BufferedInputStream(s.getInputStream());
+		//目的File对象
 		File file = new File("E:\\浙江.pptx");
+		//创建一个文件输出字节流，加上字节流缓冲区
 		BufferedOutputStream bufos = new BufferedOutputStream(new FileOutputStream(file));
+		//创建缓冲区，用于存储，从Socket字节输入流中读到的数据
 		byte [] buf = new byte[1024];
 		int len = 0;
+		//循环读取
 		while((len = bufis.read(buf))!=-1) {
+			//将读取到的数据，写入到目的流中
 			bufos.write(buf, 0, len);
-			System.out.println(len);
+			//刷新数据
 			bufos.flush();
-			byte []ref = new byte[1024];
-			if()
-				break;
 		}
 		
+		//获取Socket输出流，向客户端返回数据
 		BufferedOutputStream rebufos = new BufferedOutputStream(s.getOutputStream());
 		String finish = "文件上传完成";
 		//String fileName = file.getName();
